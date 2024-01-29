68d8c79a467c0dce66c8eb6dfa795490c050e02d
Wei Lai <wlai@liveperson.com>
2024-01-28 21:48:33 -0500

    full url is available

--- api/hello.js
+++ api/hello.js
@@ -2,24 +2,30 @@ module.exports = async (req, res) => {
   const axios = require("axios");
   const userAgent = req.headers['user-agent'];
   console.log("hello " + userAgent);
+  console.log("url host = " + req.headers.host);
+  console.log("url = " + req.url);
+  console.log("protocol = " + req.protocol);
 
-  console.log("api key is " + process.env.API_KEY);
+  const fullUrl = `${req.connection.encrypted ? 'https' : 'http'}://${req.headers.host}${req.url}`;
+  console.log(fullUrl)
 
+  // invoke another serverless 1 serverless
   let config = {
-    method: "post",
+    method: "get",
     maxBodyLength: Infinity,
-    url: "https://va.idp.liveperson.net/api/account/90412079/signup",
+    url: `${req.connection.encrypted ? 'https' : 'http'}://${req.headers.host}/api/getjwt`,
     headers: {},
   };
 
-  await axios
+  const result = await axios
     .request(config)
     .then((response) => {
       console.log(JSON.stringify(response.data));
+      return response.data
     })
     .catch((error) => {
       console.log(error);
     });
 
-  res.status(200).json({ message: "Hello, World1223!!!!" });
+  res.status(200).json(result);
 };
