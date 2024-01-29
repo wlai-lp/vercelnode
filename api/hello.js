90de8db235c7ee350191a77ad12e00305c0eb47b
Wei Lai <wlai@liveperson.com>
2024-01-28 20:31:21 -0500

    add idp call

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,23 @@
-module.exports = (req, res) => {
-    res.status(200).json({ message: 'Hello, World!!!!' });
+module.exports = async (req, res) => {
+  const axios = require("axios");
+
+  console.log("hello " + req);
+
+  let config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: "https://va.idp.liveperson.net/api/account/90412079/signup",
+    headers: {},
   };
+
+  await axios
+    .request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+  res.status(200).json({ message: "Hello, World1223!!!!" });
+};
