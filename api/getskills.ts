55bbbd61a3aa0b437a9f65fbc74032284aa0e89d
Wei Lai <wlai@liveperson.com>
2024-01-30 01:29:12 -0500

    working get skills

--- api/getskills.ts
+++ api/getskills.ts
@@ -1,63 +1,81 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { parse } from 'cookie'
+import axios from 'axios';
 
-export default (req: VercelRequest, res: VercelResponse) => {
+interface SkillType {
+    skillOrder: number;
+    deleted: boolean;
+    canTransfer: boolean;
+    maxWaitTime: number;
+    skillRoutingConfiguration: any[]; // Replace 'any' with a more specific type if known
+    name: string;
+    description: string;
+    fallbackWhenAllAgentsAreAway: boolean;
+    id: number;
+    dateUpdated: string;
+    postConversationSurveyAppInstallAssociationId: string;
+  }
 
+export default async (req: VercelRequest, res: VercelResponse) => {
+
+    console.log(`Get skills from ${req.query.param}`);
     // const lpSourceSite = req.cookies['LPsoucesiteid'];
     // const lpDestSite = req.cookies['LPdestiteid'];
 
     // console.log(req.headers.cookie);
     const rawCookieHeader = req.headers.cookie || '';
 
-    console.log(req.query.param);
+    const cookies = parse(rawCookieHeader);
+    const siteId = req.query.param == "source" ? cookies['LPsourcesiteid'] : cookies['LPdestiteid']
+    const token = req.query.param == "dest" ? cookies['LPsourcetoken'] : cookies['LPsourcetoken']
+    console.log(`site = ${siteId} and token ${token}`);
 
     // Parse the raw cookie header using the cookie package
-    // const cookies = parse(rawCookieHeader);
     // console.log(cookies['LPsourcetoken']);
 
-  const htmlContent = `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <title>Centered Two Panels</title>
-  </head>
-  <body class="flex h-screen">
-  
-    <!-- Container for Centering -->
-    <div class="flex mx-auto">
-  
-      <!-- Left Panel -->
-      <div class="flex-none w-1/2 bg-gray-200 p-4">
-        <!-- Content for the left panel -->
-        <h1 class="text-xl font-bold mb-4">Source Site Id</h1>
-        <p>This is the left panel content.</p>
-        <div hx-get="/api/" hx-trigger="load">
-            <img  alt="Result loading..." class="htmx-indicator" width="150" src="/img/bars.svg"/>
-        </div>
-      </div>
-  
-      <!-- Right Panel -->
-      <div class="flex-grow bg-gray-300 p-4">
-        <!-- Content for the right panel -->
-        <h1 class="text-xl font-bold mb-4">Destination Site ID</h1>
-        <p>This is the right panel content.</p>
-      </div>
-  
-    </div>
-  
-  </body>
-  </html>
+  const skillCardTemplate = `
+  <!-- Small Card -->
+<div class="bg-white p-6 my-4 rounded-lg shadow-md">
+  <!-- Card Content -->
+  <h2 class="text-lg font-semibold mb-2">{name}</h2>
+  <p class="text-gray-600 text-sm">{description}</p>
+</div>
   
   `;  
 
+  // login and get auth token
+  const config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: `https://va.ac.liveperson.net/api/account/${siteId}/configuration/le-users/skills?v=2.0&source=ccuiUm&select=$all`,
+    headers: { 
+        'authorization': `Bearer ${token}`, 
+        'content-type': 'application/json; charset=utf-8', 
+        'Cookie': 'JSESSIONID=B640FAC3AA64EF4DB6215710DAE0857A'
+      }
+  };
+
+  let htmlContent = "";
+  try {
+    console.log('start get skills request');
+    const response = await axios.request(config);
+    console.log('done get skills request');
+    const skills: SkillType[] = response.data.map((data) => ({
+        name: data.name,
+        description: data.description
+    }));
+    console.log(skills.length);
+    const skillCards: string[] = skills.map((skill) => {
+        return skillCardTemplate.replace("{name}", skill.name).replace("{description}", skill.description);
+    });
+    htmlContent = skillCards.join(" ");
+    // console.log(JSON.stringify(response.data));
+    res.setHeader('Content-Type', 'text/html');
+
+  } catch (error) {
+    console.error(error);
+    // res.status(500).json({ error: 'Internal Server Error2' });
+  }
   res.status(200).send(htmlContent);
 
-  // const { name = 'World' } = query;
-
-  // return res.json({
-  //   message: `Hello ${name}!`,
-  // });
 };
