9f0fb71668b1ed986595be51830ac1f91357202d
Wei Lai <wlai@liveperson.com>
2024-01-31 01:34:05 -0500

    fixed table hover display issue

--- api/compare.ts
+++ api/compare.ts
@@ -15,78 +15,113 @@ export default (req: VercelRequest, res: VercelResponse) => {
     // TODO: error handling site cookies param
 
   const htmlContentTemplate = `
-  <html data-theme="light">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/daisyui@4.6.1/dist/full.min.css" rel="stylesheet" type="text/css" />
-  <script src="https://cdn.tailwindcss.com"></script>
-  <script>
-    tailwind.config = {
-    	plugins: [require("daisyui")],
-      theme: {
-        extend: {
-          colors: {
-            clifford: '#da373d',
-          }
-        }
-      }
-    }
-  </script>
-  <style type="text/tailwindcss">
-    @layer utilities {
-      .content-auto {
-        content-visibility: auto;
-      }
-    }
-  </style>
-  <script src="https://unpkg.com/htmx.org@1.9.10"></script>
+<html data-theme="light">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/daisyui@4.6.1/dist/full.min.css"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <script src="https://unpkg.com/htmx.org@1.9.10"></script>
     <title>Compare 2 LP Sites</title>
-</head>
+  </head>
 
   <body>
     <div class="grid grid-cols-2 gap-4 p-8">
       <!-- Panel 1 -->
-      <div class="bg-white p-4">
-        <h2 class="text-lg font-semibold mb-2">Source SiteID : {lpSourceSite}</h2>
-        <details class="collapse bg-base-200">
-          <summary class="collapse-title text-xl font-medium">Skills</summary>
+      <div class="join join-vertical w-full">
+        <h2 class="text-lg font-semibold mb-2">
+          Source SiteID : {lpSourceSite}
+        </h2>
+        <div class="collapse collapse-arrow join-item border border-base-300">
+          <input type="radio" name="my-accordion-4" checked="checked" />
+          <div class="collapse-title text-xl font-medium">Skills</div>
           <div class="collapse-content">
-            <div hx-get="/api/skill/getskills?param=source" hx-trigger="load">
-              <img
-                alt="Result loading..."
-                class="htmx-indicator"
-                width="150"
-                src="https://htmx.org/img/bars.svg"
-              />
+            <div class="overflow-x-auto">
+              <div hx-get="/api/skill/getskills?param=source" hx-trigger="load">
+                <img
+                  alt="Result loading..."
+                  class="htmx-indicator"
+                  width="150"
+                  src="https://htmx.org/img/bars.svg"
+                />
+              </div>
             </div>
           </div>
-        </details>
+        </div>
+        <div class="collapse collapse-arrow join-item border border-base-300">
+          <input type="radio" name="my-accordion-4" />
+          <div class="collapse-title text-xl font-medium">
+            Click to open this one and close others
+          </div>
+          <div class="collapse-content">
+            <p>hello</p>
+          </div>
+        </div>
+        <div class="collapse collapse-arrow join-item border border-base-300">
+          <input type="radio" name="my-accordion-4" />
+          <div class="collapse-title text-xl font-medium">
+            Click to open this one and close others
+          </div>
+          <div class="collapse-content">
+            <p>hello</p>
+          </div>
+        </div>
       </div>
 
-      <!-- Panel 2 -->
-      <div class="bg-white p-4">
-        <h2 class="text-lg font-semibold mb-2">Desitnation SiteID : {lpDestSite}</h2>
-        <details class="collapse bg-base-200">
-        
-          <summary class="collapse-title text-xl font-medium">Skills</summary>
+      <!-- panel 2 lpDestSite -->
+      <div class="join join-vertical w-full">
+        <h2 class="text-lg font-semibold mb-2">
+          Source SiteID : {lpDestSite}
+        </h2>
+        <div class="collapse collapse-arrow join-item border border-base-300">
+          <input type="radio" name="my-accordion-5" checked="checked" />
+          <div class="collapse-title text-xl font-medium">Skills</div>
           <div class="collapse-content">
-            <div hx-trigger="myEvent from:body" hx-get="/api/skill/getskills?param=dest">
+            <div class="overflow-x-auto">
+              <div
+                hx-trigger="myEvent from:body"
+                hx-get="/api/skill/getskills?param=dest"
+              >
                 <div hx-get="/api/skill/getskills?param=dest" hx-trigger="load">
-                <img
+                  <img
                     alt="Result loading..."
                     class="htmx-indicator"
                     width="150"
                     src="https://htmx.org/img/bars.svg"
-                />
+                  />
                 </div>
+              </div>
             </div>
           </div>
-        </details>
+        </div>
+        <div class="collapse collapse-arrow join-item border border-base-300">
+          <input type="radio" name="my-accordion-5" />
+          <div class="collapse-title text-xl font-medium">
+            Click to open this one and close others
+          </div>
+          <div class="collapse-content">
+            <p>hello</p>
+          </div>
+        </div>
+        <div class="collapse collapse-arrow join-item border border-base-300">
+          <input type="radio" name="my-accordion-5" />
+          <div class="collapse-title text-xl font-medium">
+            Click to open this one and close others
+          </div>
+          <div class="collapse-content">
+            <p>hello</p>
+          </div>
+        </div>
       </div>
     </div>
   </body>
 </html>
+
 
   
   `;  
